0e910b5bfe47ac134141dd36dc16e475e0056b71
ljenya <61124137+ljenya@users.noreply.github.com>
2020-03-08 19:55:16 -0700

    Initial code

---
+++ app.js
@@ -0,0 +1,44 @@
+//Use express and handlebars
+//var PORT = process.env.PORT || 8009;
+var express = require('express');
+var mysql = require('./dbcon.js');
+var morgan = require('morgan');
+var app = express();
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use('/', express.static('public'));
+app.set('mysql', mysql);
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+
+//Render the home page
+app.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+//Page rendering
+app.use('/weather', require('./public/js/weather.js'));
+
+//Not found error page rendering
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+//listen on specified port
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
