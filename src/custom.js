25185214bc00fac269fb4c6699b71063b08c26ce
Logan Saso <logansaso+tech@gmail.com>
2020-03-09 15:57:49 -0700

    Trimmed a bit of extra fat

--- src/custom.js
+++ src/custom.js
@@ -7,17 +7,6 @@ module.exports = function () {
     var async = require('async');
 
 
-    //Get the weather table
-    function getweather(mysql) {
-        return function (callback) {
-            mysql.pool.query("SELECT State, County, Year, Good_Days, Moderate_Days, Unhealthy_Days, Hazardous_Days, Max_AQI, Median_AQI, Days_CO, Days_Ozone, Days_SO2 FROM AirQualityAndParticles", function (err, tb1) {
-                if (err) {
-                    return callback(err, []);
-                }
-                return callback(null, tb1);
-            });
-        }
-    }
 
     //Search trough the weather table based on given filters in handlebars
     function searchFunction(req, res, mysql, context, complete) {
@@ -44,65 +33,22 @@ module.exports = function () {
 
     //Render the page with the loaded tables
     router.get('/', function (req, res) {
-        var mysql = req.app.get('mysql');
-        async.parallel({
-            custom: getweather(mysql)
-        }, function (err, results) {
-            if (err) {
-                console.log(err.message);
-            }
-            res.render('custom', results);
-        });
-    });
-
-    //Render the search results based on selected criteria
-    router.get('/customQuery', function (req, res) {
         var callbackCount = 0;
         var context = {};
-        context.firstQuery = req.query.query || "";
+        context.query = req.query.query || "";
         var mysql = req.app.get('mysql');
-        customQuery(req, res, mysql, context, req.query.query, complete);
-
+        const query = req.query.query;
+        if (!query) {
+            res.render('custom', context);
+            return;
+        }
         function complete() {
             callbackCount++;
             if (callbackCount >= 1) {
                 res.render('custom', context);
             };
         };
-    });
-
-    //Update a row from weather table baed on id
-    router.post('/update', function (req, res) {
-        console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "UPDATE weather SET priority = ?, member_name = ?, note = ? WHERE id = ?";
-        var inserts = [req.body.editPriority, req.body.editMember, req.body.editNote, req.body.updateID];
-        sql = mysql.pool.query(sql, inserts, function (err, results) {
-            if (err) {
-                console.log(JSON.stringify(err))
-                res.write(JSON.stringify(err));
-                res.end();
-            } else {
-                res.redirect('/custom');
-            }
-        });
-    });
-
-    //Delete an entry from the table based on client id
-    router.post('/delete', function (req, res) {
-        var mysql = req.app.get('mysql');
-        var sql = "DELETE FROM weather WHERE id = ?";
-        var inserts = [req.body.deleteGID];
-        sql = mysql.pool.query(sql, inserts, function (err, results) {
-            if (err) {
-                console.log(err)
-                res.write(JSON.stringify(err));
-                res.status(400);
-                res.end();
-            } else {
-                res.redirect('/custom');
-            }
-        });
+        customQuery(req, res, mysql, context, req.query.query, complete);
     });
 
     return router;
